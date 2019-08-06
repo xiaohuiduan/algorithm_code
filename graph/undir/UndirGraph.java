96f9d0a64e739ae0aaf9dba95bd5affb58912624
段小辉 <2283330272@qq.com>
2019-08-06 18:54:36 +0800

    添加无向图

---
+++ graph/undir/UndirGraph.java
@@ -0,0 +1,88 @@
+package graph.undir;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: xiaohuiduan
+ * @Date: 19-8-5 下午4:00
+ * _ooOoo_
+ * o8888888o
+ * 88" . "88
+ * (| -_- |)
+ * O\  =  /O
+ * ____/`---'\____
+ * .'  \\|     |//  `.
+ * /  \\|||  :  |||//  \
+ * /  _||||| -:- |||||-  \
+ * |   | \\\  -  /// |   |
+ * | \_|  ''\---/''  |   |
+ * \  .-\__  `-`  ___/-. /
+ * ___`. .'  /--.--\  `. . __
+ * ."" '<  `.___\_<|>_/___.'  >'"".
+ * | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ * \  \ `-.   \_ __\ /__ _/   .-` /  /
+ * ======`-.____`-.___\_____/___.-`____.-'======
+ * `=---='
+ * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * 佛祖保佑       永无BUG
+ */
+public class UndirGraph extends Graph {
+
+    UndirGraph(int V) {
+        super(V);
+    }
+
+    @Override
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        this.E ++;
+    }
+
+    @Override
+    Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    /**
+     * 与结点相连通的所有结点
+     *
+     * @param s
+     * @return
+     */
+    @Override
+    Iterable<Integer> search(int s) {
+        DepthFirstSearch dfs = new DepthFirstSearch(this,s);
+        List list = new ArrayList(dfs.getCount());
+        for (int i=0;i<this.V();i++) {
+            if (dfs.getMarked(i)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 是否存在S结点到V结点的路径
+     * @param s
+     * @param v
+     * @return
+     */
+    @Override
+    boolean hasPathTo(int s, int v) {
+        return false;
+    }
+
+    /**
+     * 找出s到v结点的所有路径
+     *
+     * @param s
+     * @param v
+     * @return
+     */
+    @Override
+    Iterable<Integer> pathTo(int s, int v) {
+        return null;
+    }
+}
